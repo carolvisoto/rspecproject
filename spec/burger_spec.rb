d43cdbf870c6d67ee6e7acf203f75653886056b7
Caroline Visoto Nunes <carolvisoto@gmail.com>
2016-04-12 15:18:25 +0200

    apply subject and specify

--- spec/burger_spec.rb
+++ spec/burger_spec.rb
@@ -1,21 +1,16 @@
 describe Burger do
   describe "#apply_ketchup" do
-    context "with ketchup" do
-      let(:burger){Burger.new(:ketchup => true)
-      before {burger.apply_ketchup}
+    subject {burger}
+    before {burger.apply_ketchup}
 
-      it "sets the katchup flag to true" do
-        @burger.has_ketchup_on_it?.should be_true
-      end
+    context "with ketchup" do
+      let(:burger){ Burger.new (:ketchup => true) }
+      specify{ subject.has_ketchup_on_it?.should be_true }
     end
 
     context "without ketchup" do
-      let(:burger){Burger.new (:ketchup => false)}
-      before {burger.apply_ketchup}
-      
-      it "sets teh ketchup flag to false" do
-        @burger.has_ketchup_on_it?.should be_false
-      end
+      let(:burger){ Burger.new (:ketchup => false) }
+      specify{ subject.has_ketchup_on_it?.should be_false }
     end
   end
 end
